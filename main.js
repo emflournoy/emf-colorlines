f533cc74fd04314edf943960a361be193d187ea8
Elizabeth Flournoy <bflournoy@gmail.com>
2017-07-10 14:15:25 -0600

    update speeds. background now orange at end of animation - not just border

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ $('.drip').on('animationend', ()=>{
   $('.drip2').addClass('drip2go');
   $('.drip2').on('animationend', ()=>{
     $('.drip2').addClass('slideup');
+    $('.drip2').on('animationend', ()=>{
+      $('.drip2').removeClass('drip2');
+      $('.drip2').removeClass('slideup');
+      $('.drip2').addClass('backgroundOrange');
+    });
   });
 });
